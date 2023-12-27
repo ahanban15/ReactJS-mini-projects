a87ab21d4e1ecb9ab3ccf26473f30d68160f78ac
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2023-12-27 12:14:25 +0530

    menu added
    commit

--- restaurant-website/src/Components/Basics/Restaurant.js
+++ restaurant-website/src/Components/Basics/Restaurant.js
@@ -1,31 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 import Menu from './MenuAPI'
+import MenuCard from './MenuCard';
 
 const Restaurant = () => {
 //   const colorStyle = {color: 'red'};
-
-  return (
-    <>
-    <div className="card-container">
-        <div className="card">
-            <div className="card-body">
-                <span className="card-number card-circle subtle">1</span>
-                {/* <span className="card-author subtle" style={colorStyle}>Breakfast</span> */}
-                <span className="card-author subtle">Breakfast</span>
-                <h2 className="card-title">Maggi</h2>
-                <span className="card-description subtle">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate fugiat sapiente facilis officiis maxime rem iste ipsum totam, doloribus facere qui veniam nesciunt ab tenetur quia. Hic dicta magni sed?
-                </span>
-                <div className="card-read">Read</div>
-            </div>
-            <img src={image} alt="images" className='card-media'/>
-
-            <span className="card-tag subtle">Order now</span>
-        </div>
-    </div>
-    </>
-  )
+    const [menuData, setMenuData] = useState(Menu);
+     
+    return (
+        <>
+            <MenuCard menuData = {menuData}/ >
+        </>
+    );
 }
 
 export default Restaurant
