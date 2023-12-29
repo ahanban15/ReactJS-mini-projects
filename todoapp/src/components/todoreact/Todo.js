fd1beaa2178ccc747f791c77159cbde9b73c0599
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2023-12-29 21:49:11 +0530

    initial setup
    commit

---
+++ todoapp/src/components/todoreact/Todo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./style.css"
+
+const Todo = () => {
+  return (
+    <>
+    <div className="main-div">
+        <div className="child-div">
+            <figure>
+                <img src="./images/todo.svg" alt="icon" />
+                <figcaption>Add your List Here</figcaption>
+            </figure>
+
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Todo
